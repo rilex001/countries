a522aadb5e314083823500d0b8dbc472f405b6de
rilex001 <ristic1900@gmail.com>
2020-06-20 18:07:28 +0200

    change mode button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Country from './components/Country';
 import { BrowserRouter, Switch, Route} from 'react-router-dom'
 import CountryPage from './components/CountryPage'
 
+
+
 function App() {
+
+  const [color, setColor] = useState(true)
   return (
-    <>
-      <h1 className='header'>Country</h1>
+    <div className={color ? 'background-white' : 'background-black'}>
+      <div className='headerContainer'>
+           <h1 className='header'>Where in the world</h1>
+            <button className='themebutton' onClick={() => setColor(!color)}>Change mode</button>
+      </div>
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Country} />
           <Route path="/:country_name"  component={CountryPage} />
         </Switch>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
